b21f6b26be1ddf741d0c183523244912594bd7c2
Mike Hinchey <mike.hinchey@omnisci.com>
2020-06-24 23:06:36 -0700

    test by writing output then diff to known gold

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -9,7 +9,7 @@ use omnisci_log_scraper::log_parser as olog;
 #[test]
 fn test_log_file_parse() {
     let mut test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    test_path.push("tests/test_log_file.log");
+    test_path.push("tests/omnisci_server.INFO");
     let f = File::open(test_path.as_path()).unwrap();
     let mut buf_reader = BufReader::new(f);
     let lines: Vec<Result<olog::LogLine, Error>> = olog::ParsingLine::new(&mut buf_reader).collect();
