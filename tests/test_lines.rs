b62732d0d6c1e987299242fd41760c78401968eb
Mike Hinchey <mike.hinchey@omnisci.com>
2020-10-01 09:40:38 -0700

    parse nonce to dashboard and chart columns
    add 2 columns to csv/table
    use alter table to auto upgrade
    add serde_json
    run cargo update

--- tests/test_lines.rs
+++ tests/test_lines.rs
@@ -63,9 +63,8 @@ fn sql_execute_nonce() {
     assert_eq!(rec.operation.unwrap(), "SELECT");
     assert_eq!(rec.query.unwrap(), "SELECT count(*) from omnisci_states;");
     assert_eq!(rec.client.unwrap(), "tcp:172.17.0.1:52290");
-    let name_values = rec.name_values.unwrap();
-    assert_eq!(name_values[0], r#"nonce"#);
-    assert_eq!(name_values[1], r#"{"chartId":"work_to_generate_logs","dashboardId":100}"#);
+    assert_eq!(rec.dashboardid.unwrap(), "100");
+    assert_eq!(rec.chartid.unwrap(), "work_to_generate_logs");
 }
 
 #[test]
@@ -92,7 +91,6 @@ fn render_vega_begin2() {
     assert_eq!(rec.query.unwrap(), r#"
 {"width":1002,"height":726,"viewRenderOptions":{"premultipliedAlpha":false},"data":[{"name":"pointmap","sql":"SELECT conv_4326_900913_x(st_xmin(omnisci_geo)) AS x, conv_4326_900913_y(st_ymin(omnisci_geo )) AS y FROM omnisci_states WHERE ((st_xmin(omnisci_geo) is not null\n          AND st_ymin(omnisci_geo ) is not null\n          AND st_xmin(omnisci_geo) >= -178.12315200000032 AND st_xmin(omnisci_geo) <= -67.26987899999968 AND st_ymin(omnisci_geo ) >= -0.8144879012842097 AND st_ymin(omnisci_geo ) <= 61.96302517868901)) LIMIT 10000000","enableHitTesting":false}],"scales":[{"name":"x","type":"linear","domain":[-19828578.576412328,-7488448.674977641],"range":"width"},{"name":"y","type":"linear","domain":[-90671.43229163112,8850380.771762503],"range":"height"},{"name":"pointmap_fillColor","type":"linear","domain":[0,0.125,0.25,0.375,0.5,0.625,0.75,0.875,1],"range":["rgba(17,95,154,0.475)","rgba(25,132,197,0.5471153846153846)","rgba(34,167,240,0.6192307692307691)","rgba(72,181,196,0.6913461538461538)","rgba(118,198,143,0.7634615384615384)","rgba(166,215,91,0.835576923076923)","rgba(201,229,47,0.85)","rgba(208,238,17,0.85)","rgba(208,244,0,0.85)"],"accumulator":"density","minDensityCnt":"-2ndStdDev","maxDensityCnt":"2ndStdDev","clamp":true}],"projections":[],"marks":[{"type":"symbol","from":{"data":"pointmap"},"properties":{"xc":{"scale":"x","field":"x"},"yc":{"scale":"y","field":"y"},"fillColor":{"scale":"pointmap_fillColor","value":0},"shape":"circle","width":5,"height":5}}]}
     "#.trim());
-    let name_values = rec.name_values.unwrap();
-    assert_eq!(name_values[4], r#"nonce"#);
-    assert_eq!(name_values[5], r#"{"chartId":"work_to_generate_logs","dashboardId":100}"#);
+    assert_eq!(rec.dashboardid.unwrap(), "100");
+    assert_eq!(rec.chartid.unwrap(), "work_to_generate_logs");
 }
